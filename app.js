5ee7e6697af7f833815331b406a31ed73c36bb28
Elmedina Menxhiqi <elmedinamenxhiqi9@gmail.com>
2024-11-30 01:05:11 +0100

    git

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const inputs=document.querySelectorAll(".contact-input");
 
-inputs.forEach(ipt =>{
+inputs.forEach((ipt) =>{
     ipt.addEventListener("focus", () =>{
-        ipt.parentNode.classList.add("focus")
+        ipt.parentNode.classList.add("focus");
+        ipt.parentNode.classList.add("not-empty");
     })
 } )
-inputs.forEach(ipt =>{
+inputs.forEach((ipt)=>{
     ipt.addEventListener("blur", () =>{
-        ipt.parentNode.classList.remove("focus")
+        if(ipt.value==""){
+            ipt.parentNode.classList.remove("not-empty");
+        }
+        ipt.parentNode.classList.remove("focus");
     })
 } )
