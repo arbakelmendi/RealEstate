247a2893ced8e1e5de9760f6e5ca5099412d7750
arbakelmendi <119315754+arbakelmendi@users.noreply.github.com>
2024-12-01 17:33:47 +0100

    .

--- js/login.js
+++ js/login.js
@@ -1,14 +1,14 @@
 //function to check if user exists and has typed correct password
 function validateUser(){
-    let username = document.getElementById("username").value; // Arba
-    let password = document.getElementById("password").value; // Password123
+    let username = document.getElementById("username").value; 
+    let password = document.getElementById("password").value; 
     
     //validate 
-    if(username=="Arba" & password == "Password123" ) {
+    if(username=="Admin" & password == "Admin123" ) {
         alert("Bravo o KING!")
     }
     else {
-        alert("Lem Rahaat aaabi")
+        alert("Gabim!")
     }
 }
 
