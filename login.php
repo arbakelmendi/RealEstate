5d59145ad6e5856a80ffdb32ec66c1074e7b5a19
arbakelmendi <119315754+arbakelmendi@users.noreply.github.com>
2025-01-08 00:19:54 +0100

    Check the user and password

--- login.php
+++ login.php
@@ -6,42 +6,61 @@
 // define POST on login.php
 // check if access with post
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collect value of username & password field
     $username = $_POST['username'];
-    if (empty($username)) {
-        echo "Name is empty";
-    } else {
-        $servername = "127.0.0.1";
-        $usernameDB = "root";
-        $password = "";
-        $db = "RealEstate";
-
-        // Create connection
-        $conn = new mysqli($servername,$usernameDB, $password , $db);
-
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        echo "Connected successfully";
-        
-        
-    
-            $sql="SELECT * FROM `users` WHERE username = '$username';";
-            $query = mysqli_query($conn,$sql);
-            $result = mysqli_fetch_assoc($query);
-            $resultstring = $result['message'];
-        
-            echo $resultstring;
-    }
-
     $password = $_POST['password'];
-    if (empty($password)) {
-        echo "PASSWORD is empty";
-    } else {
-        echo $password;
+
+    $servername = "127.0.0.1";
+    $usernameDB = "root";
+    $passwordDB = "";
+    $db = "RealEstate";
+
+    // Create connection
+    $conn = new mysqli($servername, $usernameDB, $passwordDB, $db);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully</br>";
+
+
+    // check user 
+    $sql = "SELECT * FROM `users` WHERE username = '$username'";
+
+    $query = mysqli_query($conn, $sql); // sqlobject 
+
+    if ($query != false) {
+
+
+        $result = mysqli_fetch_assoc($query); // turn to assoc array
+        // ['username' =>'Arba','password'=>'arba123','status'=>'user','id'=> 8724]
+
+        if ($result) {
+            // Access the 'username' field from the result
+            $resultstring = $result['username'];
+            echo $resultstring;
+        } else {
+            echo "No user found with username: $username";
+        }
+
+        // check pw of user
+        $sqlPw = "SELECT pw FROM `users` WHERE username = '$username' AND pw = '$password';";
+
+        $queryPw = mysqli_query($conn, $sqlPw);
+
+        if ($queryPw != false) {
+
+            $resultPw = mysqli_fetch_assoc($queryPw); // turn to assoc array
+            // ['password'=>'arba123']
+
+            if (!$resultPw) {
+                echo "The password is incorrect!";
+            }
+        }
     }
 }
+
+
 ?>
 
 <head>
@@ -56,9 +75,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 <body>
-
-
-
 
     <div class="banner">
         <div class="navbar">
